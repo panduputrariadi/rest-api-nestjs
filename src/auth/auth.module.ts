7c6e450932dd344d1b094b880a7e2b1c1e45c84a
panduputragit <put.rariadi1144@gmail.com>
2024-07-20 13:07:17 +0800

    user and protected routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PrismaModule, JwtModule],
+  imports: [PrismaModule, JwtModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
